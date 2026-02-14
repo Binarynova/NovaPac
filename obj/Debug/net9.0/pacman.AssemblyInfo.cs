65fc2de6f54d60b2046c596f2882c118eb2199e4
Thomas Knight <binarynova@gmail.com>
2026-02-14 01:00:34 -0600

    first inklings of tile decoding

--- obj/Debug/net9.0/pacman.AssemblyInfo.cs
+++ obj/Debug/net9.0/pacman.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pacman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9100a8a1c14ab712a0bd3891fea35bb7f905160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7a263549df3b84a09ee8c5a27b95e4abc7a1d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("pacman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pacman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
