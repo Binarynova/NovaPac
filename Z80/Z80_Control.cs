4d939a642922147f7932a2917dfa6ba67a83c6f1
Thomas Knight <binarynova@gmail.com>
2026-03-13 15:04:02 -0500

    Changed swap to tuples.

--- Z80/Z80_Control.cs
+++ Z80/Z80_Control.cs
@@ -10,15 +10,9 @@ public partial class Z80
     
     private static int Op_EXX()
     {
-        ushort temp = Reg.BC;
-        ushort temp2 = Reg.DE;
-        ushort temp3 = Reg.HL;
-        Reg.BC = Reg.BC2;
-        Reg.DE = Reg.DE2;
-        Reg.HL = Reg.HL2;
-        Reg.BC2 = temp;
-        Reg.DE2 = temp2;
-        Reg.HL2 = temp3;
+        (Reg.BC, Reg.DE, Reg.HL, Reg.BC2, Reg.DE2, Reg.HL2) = 
+            (Reg.BC2, Reg.DE2, Reg.HL2, Reg.BC, Reg.DE, Reg.HL);
+        
         Reg.PC += 1;
         return 4;
     }
