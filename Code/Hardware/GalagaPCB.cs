f4f80b7296658c60423faf9ad062cb16c0303ad6
Thomas Knight <binarynova@gmail.com>
2026-04-10 18:23:11 -0500

    sub and sub2 ROMs loaded into memory.

--- Code/Hardware/GalagaPCB.cs
+++ Code/Hardware/GalagaPCB.cs
@@ -8,7 +8,9 @@ public class GalagaPCB : IMemoryProvider
     private Z80Cpu maincpu, sub, sub2;
     private NamcoWSG soundChip;
 
-    private byte[] Memory = new byte[0x10000];
+    private byte[] MainCPUMemory = new byte[0x10000];
+    private byte[] SubCPUMemory = new byte[0x10000];
+    private byte[] Sub2CPUMemory = new byte[0x10000];
 
     public GalagaPCB(string romFileName, bool twoPlayerScreenFlip)
     {
@@ -33,7 +35,7 @@ public class GalagaPCB : IMemoryProvider
 
     public void ClearRAM()
     {
-        Array.Clear(Memory, 0, Memory.Length);
+        Array.Clear(MainCPUMemory, 0, MainCPUMemory.Length);
     }
 
     private void LoadROM(string romFileName)
@@ -48,6 +50,15 @@ public class GalagaPCB : IMemoryProvider
                 { "gg1_3.2m", 0x2000 },
                 { "gg1_4b.2l", 0x3000 }
             };
+            var romMap2 = new Dictionary<string, int>
+            {
+                { "gg1_5b.3f", 0x0000 },
+
+            };
+            var romMap3 = new Dictionary<string, int>
+            {
+                { "gg1_7b.2c", 0x0000 }
+            };
 
             foreach (var entry in romMap)
             {
@@ -59,7 +70,7 @@ public class GalagaPCB : IMemoryProvider
                     byte[] buffer = new byte[romEntry.Length];
                     s.ReadExactly(buffer, 0, buffer.Length);
                     
-                    Buffer.BlockCopy(buffer, 0, Memory, entry.Value, buffer.Length);
+                    Buffer.BlockCopy(buffer, 0, MainCPUMemory, entry.Value, buffer.Length);
                 }
 
                 else
@@ -67,7 +78,44 @@ public class GalagaPCB : IMemoryProvider
                     throw new FileNotFoundException($"Required ROM file {entry.Key} not found in zip.");
                 }
             }
-            Console.WriteLine("ROM read into Memory.");
+            foreach (var entry in romMap2)
+            {
+                ZipArchiveEntry romEntry = archive.GetEntry(entry.Key);
+
+                if (romEntry != null)
+                {
+                    using Stream s = romEntry.Open();
+                    byte[] buffer = new byte[romEntry.Length];
+                    s.ReadExactly(buffer, 0, buffer.Length);
+                    
+                    Buffer.BlockCopy(buffer, 0, SubCPUMemory, entry.Value, buffer.Length);
+                }
+
+                else
+                {
+                    throw new FileNotFoundException($"Required ROM file {entry.Key} not found in zip.");
+                }
+            }
+            
+            foreach (var entry in romMap3)
+            {
+                ZipArchiveEntry romEntry = archive.GetEntry(entry.Key);
+
+                if (romEntry != null)
+                {
+                    using Stream s = romEntry.Open();
+                    byte[] buffer = new byte[romEntry.Length];
+                    s.ReadExactly(buffer, 0, buffer.Length);
+                    
+                    Buffer.BlockCopy(buffer, 0, Sub2CPUMemory, entry.Value, buffer.Length);
+                }
+
+                else
+                {
+                    throw new FileNotFoundException($"Required ROM file {entry.Key} not found in zip.");
+                }
+            }
+            Console.WriteLine("CPU ROMs read into Memory.");
         }
     }
 }
